062272b8c75e0a8db6e3ec49048dd37af1e3fd3e
Gideon <kipkerring.gedion@gmail.com>
2023-06-21 12:50:35 -0700

    tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -17,3 +17,143 @@ class test_Amenity(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+class Test_PEP8(unittest.TestCase):
+    """test code style"""
+    def test_pep8_user(self):
+        """tests pycodestyle"""
+        pep8style = pycodestyle.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/amenity.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+class test_inherit_basemodel(unittest.TestCase):
+    """Test inheritance to the BaseModel"""
+    def test_instance(self):
+        """check if user is an instance of BaseModel"""
+        user = Amenity()
+        self.assertIsInstance(user, Amenity)
+        self.assertTrue(issubclass(type(user), BaseModel))
+        self.assertEqual(str(type(user)), "<class 'models.amenity.Amenity'>")
+
+class test_Amenity_BaseModel(unittest.TestCase):
+    """testing class amenity"""
+    def test_instance(self):
+        with patch('models.amenity'):
+            instance = Amenity()
+            self.assertEqual(type(instance), Amenity)
+            instance.name = "Barbie"
+            expected_attrs_types = {
+                    "id": str,
+                    "created_at": datetime,
+                    "updated_at": datetime,
+                    "name": str,
+                    }
+            inst_dict = instance.to_dict()
+            expected_dict_attrs = [
+                    "id",
+                    "created_at",
+                    "updated_at",
+                    "name",
+                    "__class__",
+                    ]
+            self.assertCountEqual(inst_dict.keys(), expected_dict_attrs)
+            self.assertEqual(inst_dict['name'], 'Barbie')
+            self.assertEqual(inst_dict['__class__'], 'Amenity')
+            for attr, types in expected_attrs_types.items():
+                with self.subTest(attr=attr, typ=types):
+                    self.assertIn(attr, instance.__dict__)
+                    self.assertIs(type(instance.__dict__[attr]), types)
+                    self.assertEqual(instance.name, "Barbie")
+
+    def test_user_id_and_created_at(self):
+        """test each user id created"""
+        user_1 = Amenity()
+        sleep(2)
+        user_2 = Amenity()
+        sleep(2)
+        user_3 = Amenity()
+        sleep(2)
+        list_users = [user_1, user_2, user_3]
+        for instance in list_users:
+            user_id = instance.id
+            with self.subTest(user_id=user_id):
+                self.assertIs(type(user_id), str)
+        self.assertNotEqual(user_1.id, user_2.id)
+        self.assertNotEqual(user_1.id, user_3.id)
+        self.assertNotEqual(user_2.id, user_3.id)
+        self.assertTrue(user_1.created_at <= user_2.created_at)
+        self.assertTrue(user_2.created_at <= user_3.created_at)
+        self.assertNotEqual(user_1.created_at, user_2.created_at)
+        self.assertNotEqual(user_1.created_at, user_3.created_at)
+        self.assertNotEqual(user_2.created_at, user_3.created_at)
+
+    def test_str_method(self):
+        """tests string magic method"""
+        inst = Amenity()
+        str_output = "[Amenity] ({}) {}".format(inst.id, inst.__dict__)
+        self.assertEqual(str_output, str(inst))
+
+    @patch('models.storage')
+    def test_save_method(self, mock_storage):
+        """Testing save method and if it updates"""
+        instance5 = Amenity()
+        created_at = instance5.created_at
+        sleep(2)
+        updated_at = instance5.updated_at
+        instance5.save()
+        new_created_at = instance5.created_at
+        sleep(2)
+        new_updated_at = instance5.updated_at
+        self.assertNotEqual(updated_at, new_updated_at)
+        self.assertEqual(created_at, new_created_at)
+        self.assertTrue(mock_storage.save.called)
+
+class TestAmenity(unittest.TestCase):
+    """Tests the amenity class"""
+
+    def test_is_subclass(self):
+        """Tests if amenity is a subclass of BaseModel"""
+        amenity = Amenity()
+        self.assertIsInstance(amenity, BaseModel)
+        self.assertTrue(hasattr(amenity, "id"))
+        self.assertTrue(hasattr(amenity, "created_at"))
+        self.assertTrue(hasattr(amenity, "updated_at"))
+
+    def test_name_attr(self):
+        """Tests that amenity has a name attr and is not an empty string"""
+        amenity = Amenity()
+        self.assertTrue(hasattr(amenity, "name"))
+        if storage_t == 'db':
+            self.assertEqual(amenity.name, None)
+        else:
+            self.assertEqual(amenity.name, "")
+
+    def test_to_dict_creates_dict(self):
+        """tests to_dict method creates a dictionary with proper attr"""
+        amenity = Amenity()
+        print(amenity.__dict__)
+        new_dict = amenity.to_dict()
+        self.assertEqual(type(new_dict), dict)
+        self.assertFalse("_sa_instance_state" in new_dict)
+        for attr in amenity.__dict__:
+            if attr is not "_sa_instance_state":
+                self.assertTrue(attr in new_dict)
+        self.assertTrue("__class__" in new_dict)
+
+    def test_to_dict_values(self):
+        """test that values in dict are correct"""
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        amenity = Amenity()
+        new_dict = amenity.to_dict()
+        self.assertEqual(new_dict["__class__"], "Amenity")
+        self.assertEqual(type(new_dict["created_at"]), str)
+        self.assertEqual(type(new_dict["updated_at"]), str)
+        self.assertEqual(new_dict["created_at"], amenity.created_at.strftime(t_format))
+        self.assertEqual(new_dict["updated_at"], amenity.updated_at.strftime(t_format))
+
+    def test_str(self):
+        """test that the str method has the correct output"""
+        amenity = Amenity()
+        string = "[Amenity] ({}) {}".format(amenity.id, amenity.__dict__)
+        self.assertEqual(string, str(amenity))
