df34da70b48666523d5b9c60f7a7d8a4a9591bfb
Gideon <kipkerring.gedion@gmail.com>
2023-06-24 17:36:31 -0700

    state.py

--- models/state.py
+++ models/state.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
-from models import storage
+import models
 from models.city import City
 import shlex
 
@@ -15,23 +15,20 @@ class State(BaseModel, Base):
         name: input name
     """
     __tablename__ = "states"
-    if storage == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade='all, delete, delete-orphan',
-                              backref="state")
-    else:
-        name = ''
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-        @property
-        def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-            '''
-            from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
-                if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+    @property
+    def cities(self):
+        '''returns the list of City instances with state_id
+            equals the current State.id
+            FileStorage relationship between State and City
+        '''
+        from models import storage
+        related_cities = []
+        cities = storage.all(City)
+        for city in cities.values():
+            if city.state_id == self.id:
+                related_cities.append(city)
+        return related_cities
