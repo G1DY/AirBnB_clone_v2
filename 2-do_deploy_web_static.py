3f2bdf81dfc2e1987155721207b5c776a16994e3
Gideon <kipkerring.gedion@gmail.com>
2023-07-08 05:48:13 -0700

    fabric to deploy our archive files

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,8 @@ from fabric.api import put
 from fabric.api import run
 
 env.hosts = ["52.87.255.0", "52.3.244.163"]
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
@@ -16,33 +18,24 @@ def do_deploy(archive_path):
     Returns:
         True if successful otherwise an error
     """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+    try:
+        if not (path.exists(archive_path)):
+            return False
+        put(archive_path, '/tmp/')
+        timestamp = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/releases/web_static_{}/'.
+            format(timestamp))
+        run('sudo tar -xzf /tmp/web_static_{}.tgz -C /data/web_static/ \
+            releases/web_static_{}/'..format(timestamp, timestamp))
+        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+            /data/web_static/releases/web_static_{}/'.format
+            (timestamp, timestamp))
+        run('sudo rm -rf /data/web_static/releases/ \
+            web_static_{}/web_static'.format(timestamp))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s /data/web_static/releases/ \
+            web_static_{}/ /data/web_static/current'.format(timestamp))
+    except Exception as e:
         return False
     return True
