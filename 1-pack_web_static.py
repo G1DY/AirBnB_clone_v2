6ba4228d04b49098d48bd7795a4e86e371d75222
Gideon <kipkerring.gedion@gmail.com>
2023-07-08 01:00:47 -0700

    a Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+"""a fabricfile that generates .tgz archive"""
 from fabric.api import local
 from datetime import datetime
+
 
 def do_pack():
     """Generates tgz archive from contents of web_static folder"""
