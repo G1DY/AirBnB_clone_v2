58335f8afdf686aad890aca51a0f48d42daa3155
Gideon <kipkerring.gedion@gmail.com>
2023-07-08 02:34:25 -0700

    a Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo, using the function do_pack.

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """a fabricfile that generates .tgz archive"""
+import os.path
 from fabric.api import local
 from datetime import datetime
 
@@ -17,9 +18,23 @@ def do_pack():
     if result.succeeded:
         file_size_output = result.stdout
         print(file_size_output)
-        file_size = result.stdout.split()[-2]
-        print("web_static packed: {} -> {}Bytes".format(archive_path,
-              file_size))
-        return "versions/web_static_{}.tgz".format(archive_path)
+        lines = file_size_output.split("\n")
+        file_size = None
+        for line in lines:
+            if "web_static" in line:
+                file_info = line.split()
+                if len(file_info) >= 3:
+                    file_size = file_info[2]
+                break
+        
+        if file_size:
+            print("web_static packed: {} -> {}Bytes".format(archive_path, file_size))
+            print("List of files added to the archive:")
+            print(file_size_output)
+            return archive_path
+        else:
+            print("Error: Unable to determine file size.")
     else:
-        return None
+        print("Error: Failed to generate the archive.")
+
+    return None
