a01530f4d7d8b1fc0782cff627cd1ad590207423
Gideon <kipkerring.gedion@gmail.com>
2023-07-08 00:30:27 -0700

    setting up nginx servers to deploy web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,33 +14,18 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 
 sudo touch /data/web_static/releases/test/index.html
-echo "Best School" > /data/web_static/releases/test/index.html
+sudo echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-printf %s "server {
-    listen 80 default_server,
-    listen [::]:80 default_server,
-    add_header X_served-By $HOSTNAME;
-    root /var/www/html/;
-    index index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-        root /var/www/html;
-        internal;
-    }
-}" > /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
